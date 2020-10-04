73c1b87071ed1cb1577d0a0caf505b4bcd08ca74
Wiktor Feduń <wikifed@gmail.com>
2020-10-04 11:39:54 +0200

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,22 @@
+//Zadanie 1
+
+var number = 1;
+var text = 'text'
+var numberAndString = 2+'test'
+var boolean = 3<2;
+var special =  null
+
+console.log(number);
+console.log(text);
+console.log(numberAndString);
+console.log(boolean);
+console.log(special);
+
+//Zadanie 2
+
+var numberOne = 5;
+var numberTwo = 3;
+var sum = 0;
+sum = numberOne + numberTwo;
+console.log(sum)
+
