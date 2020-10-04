691700a6411539a03a0e554ae9c5f1cba3c08a02
Wiktor Feduń <wikifed@gmail.com>
2020-10-04 11:57:14 +0200

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,48 @@
+//Zadanie 1
+let bool1 = true;
+let bool2 = false;
+console.log(bool1 === bool2);
+
+//Zadanie 2
+let num1=1;
+let num2=2;
+let moduloResult=0;
+
+moduloResult=num1%num2;
+console.log(moduloResult);
+
+//Zadanie 3
+let txt1 = "abc";
+let txt2 = "xyz";
+let stringsResult = "";
+
+stringsResult = txt1+txt2;
+
+console.log(stringsResult);
+
+
+//Zadanie 4
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber==someString);
+console.log(someNumber===someString);
+
+//  rezultaty są różne ponieważ == porównuje wartości a === porównuje watości i typy
+
+// Zadanie 5
+let counter = 30;
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+
+
+//Zadanie 6 
+let num1 = 1;
+let num2 = 2;
+let result = null;
+result = num1 > num2;
+console.log(result);
+
