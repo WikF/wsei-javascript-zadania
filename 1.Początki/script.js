6a1835577f27fe8647cf3e1d999f3653131a61f3
Wiktor Feduń <wikifed@gmail.com>
2020-12-06 20:09:39 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,15 @@
+//Zad 1 
+console.log("Twoje imię");
+
+//Zad 2
+// Wyświetla się wynik 
+* 12 + 32; - 44
+* 52 / 16; - 3.25
+* 5 * 2; - 10 
+
+//Zadanie 3
+///Wpisz do konsoli i sprawdź co się wyświetli:
+
+"Hello World"; - "Hello World";
+Hello World - Uncaught SyntaxError: Unexpected identifier
+"Hello World; - Uncaught SyntaxError: Invalid or unexpected token
